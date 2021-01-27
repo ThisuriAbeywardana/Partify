e45ad33bf123a427db58ccc0633102e6035a3ee2
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-01-27 19:03:15 +0530

    add header and css

--- User/bookEvent.php
+++ User/bookEvent.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Event</title>
-</head>
-<body>
+<?php
+    include('../Includes/header.php');
+?>
     <div class="container">
         <form action="../Controls/userControl.php" method="post">
             <div class="section">
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Event</title>
+    <link rel="stylesheet" href="../css/bookEvent.css">
+</head>
+<body>
+<nav>This is header</nav>
