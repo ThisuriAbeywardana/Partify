46577f5be175999ca3dfbcbe780d2a6ab5a7d3fa
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-01-27 23:43:16 +0530

    added some css

---
+++ User/dashboard.php
@@ -0,0 +1,18 @@
+<?php
+    include('../Includes/header.php');
+?>
+<div class="body">
+    <?php include('../Includes/navbar.php'); ?>
+    <link rel="stylesheet" href="../css/userDashboard.css">
+    <div class="mainUP">
+        <div class="card">
+            Events
+        </div>
+        <div class="card">
+            Vendors
+        </div>
+        <div class="card">
+            Accounts
+        </div>
+    </div>
+</div>
