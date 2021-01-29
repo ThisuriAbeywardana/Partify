d2bb57418dd1f08b19ab914294bba28f5a4ca0b9
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-01-30 03:21:08 +0530

    addedd some css

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -2,41 +2,44 @@
     session_start();
     if(!isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']!='true' && $_SESSION['userType']!='Admin'){
         header("Location: ../login.php");
-    }   
+    }
+    
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Account</title>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/header2.css">
-    
+    <link rel="stylesheet" type="text/css" href="../css/profile.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    <link rel="stylesheet" type="text/css" href="../css/adminDashboard.css">
 </head>
 <body>
-<?php include('../Includes/header2.php') ?>
-
+   
 <div class="container">
     <?php 
         include('./nav.php'); 
-        include('../Controls/general.php');
-            
-        
     ?>
-    <div class="mainUP">
-        <div class="card">
-            Events <?php echo getEventCount(); ?>
+    <div class="right">
+    <?php
+        include('../Includes/header2.php');
+        include('../Controls/general.php');
+    ?>
+        <div class="mainUP">
+            <div class="card c1">
+                <h3>Events</h3><p> <?php echo getEventCount(); ?></p>
+            </div>
+            <div class="card c2">
+                <h3>Vendors</h3> <p><?php echo getVendorCount(); ?></p>
+            </div>
+            <div class="card c3">
+                <h3>Accounts</h3><p><?php echo getUserCount(); ?></p> 
+            </div>
         </div>
-        <div class="card">
-            Vendors <?php echo getVendorCount(); ?>
-        </div>
-        <div class="card">
-            Accounts <?php echo getUserCount(); ?>
-        </div>
-    </div>
-
-
-
+    </div>    
 </div>
--- Admin/users.php
+++ Admin/users.php
@@ -13,29 +13,30 @@
     <title>Account</title>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
-    <!-- <link rel="stylesheet" type="text/css" href="../css/header2.css"> -->
+    <link rel="stylesheet" type="text/css" href="../css/header2.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
 </head>
 <body>
- <?php include('../Includes/header2.php'); ?>  
 <div class="container">
     <?php 
         include('./nav.php'); 
-        include('../Controls/general.php');
-        $users = getAllUsers();    
-        while($row=mysqli_fetch_assoc($users)){
-            $name = $row['fName'].' '.$row['lName'];
-            echo $name;
-        }
     ?>
-
+    <div class="right">
+        <?php
+        include('../Includes/header2.php');
+        
+        ?>
+        <div class="users">
+            <?php
+            include('../Controls/general.php');
+            $users = getAllUsers();    
+            while($row=mysqli_fetch_assoc($users)){
+                $name = $row['fName'].' '.$row['lName'];
+                echo $name;
+            }
+            ?>
+        </div>
+        
+    </div>
     <!-- show all users -->
-
-
-    <style type="text/css">
-
-</style>
-
-
-
-
 </div>
