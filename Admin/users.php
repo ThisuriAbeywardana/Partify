b46b158cc966068364d91ac9bbe84f0184643ca1
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-01-29 21:47:50 +0530

    admin users

--- Admin/users.php
+++ Admin/users.php
@@ -1,15 +1,32 @@
 <?php
-    // session_start();
-    // if(!isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']!='true' && $_SESSION['userType']!='Admin'){
-    //     header("Location: ../login.php");
-    // }
+    session_start();
+    if(!isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']!='true' && $_SESSION['userType']!='Admin'){
+        header("Location: ../login.php");
+    }
     include('../Includes/header.php');
     
 ?>
 <div class="container">
-    <?php include('./nav.php'); ?>
+    <?php 
+        include('./nav.php'); 
+        include('../Controls/general.php');
+        $users = getAllUsers();    
+        while($row=mysqli_fetch_assoc($users)){
+            $name = $row['fName'].' '.$row['lName'];
+            echo $name;
+
+
+        }
+    ?>
 
     <!-- show all users -->
+
+
+    <style type="text/css">
+
+</style>
+
+
 
 
 </div>
--- Controls/general.php
+++ Controls/general.php
@@ -1,6 +1,28 @@
 <?php
+    if(!isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']!='true'){
+        header("Location: ../login.php");
+    }
+    require_once('../Includes/db/dbConnection.php');
+    $db = DBConnection::getInstance();
+    $connection = $db->getConnection();
     function getUSerDetails($userId){
-        $sql="SELECT username,email,fName,lName,contactNo"; 
+        global $connection;
+        $sql="SELECT u.username,d.fName,d.lName,d.contactNo,d.address,d.email,u.userType FROM user u INNER JOIN userdetail d ON u.userId=d.userId WHERE u.userId='$userId'";
+        $result = mysqli_query($connection,$sql);
+        return $result;
+
+    }
+    function getAllUsers(){
+        global $connection;
+        $sql="SELECT d.fName,d.lName FROM userdetail d INNER JOIN user u ON u.userId=d.userId WHERE u.userType='User'";
+        $result = mysqli_query($connection,$sql);
+        return $result;
+    }
+    function getAllVendors(){
+        global $connection;
+        $sql="SELECT d.fName,d.lName FROM userdetail d INNER JOIN user u ON u.userId=d.userId WHERE u.userType=Vendor";
+        $result = mysqli_query($connection,$sql);
+        return $result;
     }
 
 ?>
