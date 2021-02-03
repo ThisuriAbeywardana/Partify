b2fe45cc3da7cfdbd285a058dc4109c7df72313a
Shanidarshi96 <shani.darshi.dr@gmail.com>
2021-02-03 13:10:07 +0530

    about us

---
+++ aboutus.php
@@ -0,0 +1,119 @@
+<?php
+echo "<link rel='stylesheet' type='text/css' href='css/aboutus.css' />";
+include('./includes/header.php');
+?>
+<div class="bgded overlay" style="background-image:url('Assets/aboutus4.JPG');"> 
+  <div id="pageintro">
+    <article>
+      <h3 class="heading">PARTIFY</h3>
+      <p>As an event organizer, We are here to join the gap between the party hosters and party services providers so as to provide an epic party experience for all party hosts and a chance for all other providers to show off their skills/talents.</p>
+    </article>
+  </div>
+</div>
+
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <section id="introblocks">
+      <div class="sectiontitle">
+        <h6 class="heading">WHAT WE PROVIDE</h6>
+      </div>
+      <ul class="nospace group">
+        <li class="one_quarter first">
+          <article>
+            <h6 class="heading">Are You Thinking of Hosting a Party?</h6>
+            <p>If so, you are in luck. We will organise your party the way you want without you having to go through all the hassle. All you got to do is to enjoy the party.</p>
+          </article>
+        </li>
+        <li class="one_quarter">
+          <article>
+            <h6 class="heading">Do you have party place or are you a Catering service provider?</h6>
+            <p>Join hands with us at Partify to deliver all expectations of an epic party to all who wish to have our service.</p>
+          </article>
+        </li>
+        <li class="one_quarter">
+          <article>
+            <h6 class="heading">Are you a Photography/Videography Freelancer?</h6>
+            <p>Register with us at partify along with a portfolio of your best work to provide all party hosters with quality memories of their epic day/night.</p>
+          </article>
+        </li>
+        <li class="one_quarter">
+          <article>
+            <h6 class="heading">Are you a skillful and talented decorator/baker?</h6>
+            <p>Help us improve the experince of our party hosts at their epic events while doing what you are best at and earning your share.</p>
+          </article>
+        </li>
+      </ul>
+    </section>
+  
+    <div class="clear"></div>
+  </main>
+</div>
+
+<div class="wrapper row3" style="background-image:url('Assets/aboutus2.jpg'); background-repeat: no-repeat; background-position: center;">
+  <main class="hoc container clear"> 
+    <section id="introblocks">
+      <div class="sectiontitle">
+        <h6 class="heading">MEET OUR TEAM</h6>
+        <p>The Crew members of the team which makes epic things happen. </p>
+      </div>
+      <ul class="nospace group">
+        <li class="one_quarter first">
+          <article>
+            <h6 class="heading">V H Abeywikrama</h6>
+            <p>Punctuality freak of the team that makes sure everything is on time. </p>
+          </article>
+        </li>
+        <li class="one_quarter">
+          <article>
+            <h6 class="heading">Yohombu Abeysinghe</h6>
+            <p>Planning mastermind of the team behind all events. </p>
+          </article>
+        </li>
+        <li class="one_quarter">
+          <article>
+            <h6 class="heading">Thisuri Abeywardhana</h6>
+            <p>Theme and decor eye of the team. </p>
+          </article>
+        </li>
+        <li class="one_quarter">
+          <article>
+            <h6 class="heading">Shanika Amarathunga</h6>
+            <p>Peoples' mind reader of the team. </p>
+          </article>
+        </li>
+      </ul>
+    </section>
+  
+    <div class="clear"></div>
+  </main>
+</div>
+
+
+
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <div class="sectiontitle">
+      <h6 class="heading">HAPPY CUSTOMER FEEDBACKS</h6>
+    </div>
+    <div id="latest" class="group">
+      <article class="one_third first"><img src="Assets/aboutus5.jpg" alt="Yeumyna Lee's Birthday">
+        <div class="excerpt">
+          <h6 class="heading">Yeumyna's 21st Bisrthday Party</h6>
+          <p>"This was the best birthday party ever and it was because of the unmatched service provided by everyone at Partify. Thank you Partify once again for making this party happen without any gaps."</p>
+        </div>
+      </article>
+      <article class="one_third"><img src="Assets/aboutus3.jpg" alt="ABC's Get together">
+        <div class="excerpt">
+          <h6 class="heading">ABC pvt(ltd)'s Get Together</h6>
+          <p>"Our heart filled gratitude towards partify for making our annual get together an experience which we never had before. We are overjoyed by the fact that partify has gone over and beyond expectations to bring us, what they call in their words "an epic party experience"."</p>
+        </div>
+      </article>
+      <article class="one_third"><img src="Assets/aboutus1.jpg" alt="Sarah Kaithyn's Wedding">
+        <div class="excerpt">
+          <h6 class="heading">Sarah Kaityn's Wedding</h6>
+          <p>"Thank you Partify for making our day to the best possible I could imagine and on such short notice. There is nothing I could complain about the day thanks to the support provided by the crew at partify will recommend partify ant hesitation."</p>
+        </div>
+      </article>
+    </div>
+  </section>
+</div>
