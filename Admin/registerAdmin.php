f5e2670ef096058fd1f2d8862d77b12bf4501a60
Yohombu Abeysinghe <yohombuda@gmail.com>
2021-02-06 16:37:16 +0530

    modified regi

--- Admin/registerAdmin.php
+++ Admin/registerAdmin.php
@@ -11,12 +11,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Admin</title>
-    <link rel="stylesheet" type="text/css" href="../css/adminRegister.css">
     <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/header2.css">
     <link rel="stylesheet" type="text/css" href="../css/profile.css">
     <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    <link rel="stylesheet" type="text/css" href="../css/adminRegister.css">
     <script src="../js/registerAdmin.js"></script>
 </head>
 <body>
@@ -32,22 +32,22 @@
             <div class = "admin" id ="admin">
             <br><br>
             <label for="email"><b>Email Address</b></label><br>
-            <input type="email" placeholder="Enter Email" name="email" id="email" required><br><br>
+            <input type="email" placeholder="Email" name="email" id="email" required><br><br>
             
             <label for="fname"><b>First Name</b></label>
-            <input type="text" placeholder="Enter First Name" name="fname" id="fname" required><br><br>
+            <input type="text" placeholder="First Name" name="fname" id="fname" required><br><br>
 
             <label for="lname"><b>Last Name</b></label>
-            <input type="text" placeholder="Enter Last Name" name="lname" id="lname" required><br><br>
+            <input type="text" placeholder="Last Name" name="lname" id="lname" required><br><br>
 
             <label for="contactno"><b>Contact Number</b></label><br>
-            <input type="text" placeholder="Enter Email" name="contactno" id="contactno" required><br><br>
+            <input type="text" placeholder="Email" name="contactno" id="contactno" required><br><br>
 
-            <label for="adminusername"><b>User Name</b></label>
-            <input type="text" placeholder="Enter User Name" name="adminusername" id="adminusername" required><br><br>
+            <label for="adminusername"><b>Username</b></label>
+            <input type="text" placeholder="Username" name="adminusername" id="adminusername" required><br><br>
 
             <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" id="psw" onkeyup='checkPassword3();' required><br><br>
+            <input type="password" placeholder="Password" name="psw" id="psw" onkeyup='checkPassword3();' required><br><br>
 
             <label for="psw_repeat"><b>Repeat Password</b></label>
             <input type="password" placeholder="Repeat Password" name="psw_repeat" id="psw_repeat" onkeyup='checkPassword3();' required><br><br>
--- register.php
+++ register.php
@@ -6,31 +6,31 @@
 </head>
 <body>
         <div class="container">
-            <h1><div style="text-align: center;">Register Here</div></h1>
+            <h1><div style="text-align: center;">Sign Up</div></h1>
             <hr>
 
             <label for="type"><b>I am a : </b></label>
-
+            <div class="custom-select" style="width:200px;">
             <select name="type" id="type" onchange="userType();">
                 <option value="">- Select Type -</option>
                 <option value="user" >User</option>
                 <option value="vendor">Service Provider</option>
             </select>
-
+            </div>
             <form method="POST" action="./Controls/registrationControl.php">
             <div class = "user form" id ="user">
             <br><br>
             <label for="useremail"><b>Email Address</b></label><br>
-            <input type="useremail" placeholder="Enter Email" name="useremail" id="useremail" required><br><br>
+            <input type="email" placeholder="Email Address" name="useremail" id="useremail" required><br><br>
 
             <label for="firstname"><b>First Name</b></label>
-            <input type="text" placeholder="Enter First Name" name="firstname" id="firstname" required>
+            <input type="text" placeholder="First Name" name="firstname" id="firstname" required>
 
             <label for="lastname"><b>Last Name</b></label>
-            <input type="text" placeholder="Enter Last Name" name="lastname" id="lastname" required>
+            <input type="text" placeholder="Last Name" name="lastname" id="lastname" required>
 
-            <label for="username"><b>User Name</b></label>
-            <input type="text" placeholder="Enter User Name" name="username" id="username" required>
+            <label for="username"><b>Usename</b></label>
+            <input type="text" placeholder="Username" name="username" id="username" required>
 
             <label for="male"><b>Gender</b></label><br>
             <input type="radio" id="male" name="gender" value="M">
@@ -38,23 +38,23 @@
             <input type="radio" id="female" name="gender" value="F">
             <label for="female">Female</label><br><br>
 
-            <label for="dob"><b>Date of Birth</b></label><br>
-            <input type="date" placeholder="Enter Date of Birth" name="dob" id="dob"><br><br>
+            <label for="dob"><b>Date of Birth :</b></label><br>
+            <input type="date" placeholder="Date of Birth" name="dob" id="dob"><br><br>
 
             <label for="usercontactno"><b>Contact Number</b></label>
-            <input type="text" placeholder="Enter Contact Number" name="usercontactno" id="usercontactno" required>
+            <input type="text" placeholder="Contact Number" name="usercontactno" id="usercontactno" required>
 
             <label for="useraddress"><b>Address</b></label>
-            <input type="text" placeholder="Enter your Address" name="useraddress" id="useraddress">
+            <input type="text" placeholder="Your Address" name="useraddress" id="useraddress">
 
             <label for="userpsw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="userpsw" id="userpsw" onkeyup='checkPassword();' required>
+            <input type="password" placeholder="Password" name="userpsw" id="userpsw" onkeyup='checkPassword();' required>
 
             <label for="userpsw_repeat"><b>Repeat Password</b></label>
             <input type="password" placeholder="Repeat Password" name="userpsw_repeat" id="userpsw_repeat" onkeyup='checkPassword();' required>
             <span id="passtext"></span>
             <hr>
-            <button type="submit" class="userRegisterBtn" name="userRegisterBtn" id="userRegisterBtn">Register</button>
+            <button type="submit" class="btn userRegisterBtn" name="userRegisterBtn" id="userRegisterBtn">Register</button>
             </div>
             </form>
             
@@ -62,8 +62,8 @@
             <div class = "vendor form" id="vendor">
                 <br><br>
 
-                <label for="vendorusername"><b>User Name</b></label>
-                <input type="text" placeholder="Enter your User Name" name="vendorusername" id="vendorusername" required>
+                <label for="vendorusername"><b>Username</b></label>
+                <input type="text" placeholder="Username" name="vendorusername" id="vendorusername" required>
 
                 <label for="name"><b>Name of my Business</b></label>
                 <input type="text" placeholder="Enter your Business Name" name="businessname" id="name" required>
@@ -84,12 +84,12 @@
                 <input type="password" placeholder="Repeat Password" name="vendorpsw_repeat" id="vendorpsw_repeat" onkeyup='checkPassword2();' required>
                 <span id="passtext2"></span>
                 <hr>
-                <button type="submit" class="vendorRegisterBtn" name="vendorRegisterBtn" id="vendorRegisterBtn">Register</button>
+                <button type="submit" class="btn vendorRegisterBtn" name="vendorRegisterBtn" id="vendorRegisterBtn">Register</button>
                 </div>
                 </form>
             <!-- <button type="submit" class="registerbtn" name="registerbtn">Register</button> -->
 
-            <p><i>Already have an account?</i> <a href="../login/login.html">Log in</a>.</p>
+            <p><i>Already have an account?</i> <a href="../login.php">Log in</a>.</p>
 
         </div>
 
