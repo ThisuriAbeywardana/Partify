16e473c27f4650f7142d04f7f3c2b3118084e878
Yohombu Abeysinghe <yohombuda@gmail.com>
2021-02-07 02:10:01 +0530

    updated gallery

--- gallery.php
+++ gallery.php
@@ -1,29 +1,23 @@
+<div class="headerr"><?php include('./includes/header.php'); ?></div>
 <!DOCTYPE html>
 <html lang="en">
    <head>
       <meta charset="UTF-8">
-      <!-- <link rel="stylesheet" type="text/css" href="../css/gallery.css"> -->
+      <link rel="stylesheet" type="text/css" href="./css/gallery.css">
       <title>Image Gallery</title>
       
    </head>
    <body>
 
-      <div class="header"><?php include('../includes/header.php'); ?> </div>
       
       <h1>PARTIFY GALLERY</h1>
-      <div class=""
       <div class="ourservices0">
          <h1>Birthday Parties</h1>
-      <div class="gallery">
-         <a target="_blank" href="../Assets/img/birthday/b1.jpg">
-         <img src="../Assets/img/birthday/b1.jpg">
-         </a>
-         <div class="dds">Birthday Parties</div>
-      </div>
+      
 
       <div class="gallery">
-         <a target="_blank" href="../Assets/img/decoration/d1.png">
-         <img src="../Assets/img/decoration/d1.png">
+         <a target="_blank" href="./Assets/img/decoration/d1.jpg">
+         <img src="./Assets/img/decoration/d1.jpg">
          </a>
          <div class="dds">Decorations</div>
       </div>
@@ -31,14 +25,14 @@
       <div class="ourservices1">
          <h1>Catering Service</h1>
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/catering/c1.jpg">
-            <img src="../Assets/img/catering/c1.jpg" alt="Forest">
+            <a target="_blank" href="./Assets/img/catering/c1.jpg">
+            <img src="./Assets/img/catering/c1.jpg" alt="Forest">
             </a>
             <div class="dds">Catering Services</div>
          </div>
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/catering/c2.jpg">
-            <img src="../Assets/img/catering/c2.jpg">
+            <a target="_blank" href="./Assets/img/catering/c2.jpg">
+            <img src="./Assets/img/catering/c2.jpg">
             </a>
             <div class="dds">Catering Services</div>
          </div>
@@ -49,28 +43,22 @@
          <h1>Reception Hall</h1>
          
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/reception/r1.jpg">
-            <img src="../Assets/img/reception/r1.jpg">
-            </a>
-            <div class="dds">Reception Hall</div>
-         </div>
-         <div class="gallery">
-            <a target="_blank" href="../Assets/img/reception/r2.jfif">
-            <img src="../Assets/img/reception/r2.jfif">
+            <a target="_blank" href="./Assets/img/reception/r1.jpg">
+            <img src="./Assets/img/reception/r1.jpg">
             </a>
             <div class="dds">Reception Hall</div>
          </div>
 
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/reception/r3.jpg">
-            <img src="../Assets/img/reception/r3.jpg">
+            <a target="_blank" href="./Assets/img/reception/r3.jpg">
+            <img src="./Assets/img/reception/r3.jpg">
             </a>
             <div class="dds">Reception Hall</div>
          </div>
 
          <div class="gallery">
             <a target="_blank" href="../Assets/img/wedding/w1.jpg">
-            <img src="../Assets/img/wedding/w1.jpg">
+            <img src="./Assets/img/wedding/w1.jpg">
             </a>
             <div class="dds">Decoration</div>
          </div>
@@ -78,20 +66,20 @@
       <div class="ourservices3">
          <h1>Photography & Videography</h1>
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/photovideography/p1.jpg">
-            <img src="../Assets/img/photovideography/p1.jpg">
+            <a target="_blank" href="./Assets/img/photovideography/p1.jpg">
+            <img src="./Assets/img/photovideography/p1.jpg">
             </a>
             <div class="dds">Photography</div>
          </div>
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/photovideography/p2.jpg">
-            <img src="../Assets/img/photovideography/p2.jpg">
+            <a target="_blank" href="./Assets/img/photovideography/p2.jpg">
+            <img src="./Assets/img/photovideography/p2.jpg">
             </a>
             <div class="dds">Photography</div>
          </div>
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/photovideography/p3.jpg">
-            <img src="../Assets/img/photovideography/p3.jpg">
+            <a target="_blank" href="./Assets/img/photovideography/p3.jpg">
+            <img src="./Assets/img/photovideography/p3.jpg">
             </a>
             <div class="dds">Videography</div>
          </div>
@@ -100,22 +88,38 @@
       <h1>Wedding Service</h1>
       
       <div class="gallery">
-         <a target="_blank" href="../Assets/img/wedding/w2.jpg">
-         <img src="../Assets/img/wedding/w2.jpg">
+         <a target="_blank" href="./Assets/img/wedding/w2.jpg">
+         <img src="./Assets/img/wedding/w2.jpg">
          </a>
          <div class="dds">Wedding Cake</div>
       </div>
       <div class="gallery">
-         <a target="_blank" href="../Assets/img/wedding/w3.jpg">
-         <img src="../Assets/img/wedding/w3.jpg">
+         <a target="_blank" href="./Assets/img/wedding/w3.jpg">
+         <img src="./Assets/img/wedding/w3.jpg">
          </a>
          <div class="dds">Weddings</div>
          <div class="gallery">
-            <a target="_blank" href="../Assets/img/wedding/w4.jpg">
-            <img src="../Assets/img/wedding/w4.jpg">
+            <a target="_blank" href="./Assets/img/wedding/w4.jpg">
+            <img src="./Assets/img/wedding/w4.jpg">
             </a>
             <div class="dds">Weddings</div>
          </div>
       </div>
+
+         
+   <div class="grid-container">
+      <div class="grid-item"><img src="./Assets/img/decoration/d1.jpg"></div>
+      <div class="grid-item">2</div>
+      <div class="grid-item">3</div>  
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+      <div class="grid-item">6</div>  
+      <div class="grid-item">7</div>
+      <div class="grid-item">8</div>
+      <div class="grid-item">9</div>  
+   </div>
+
+      
+  
    </body>
 </html
--- includes/db/dbConnection.php
+++ includes/db/dbConnection.php
@@ -4,7 +4,7 @@
         private static $_instance; //The single instance
         private $_host = "localhost";
         private $_username = "root";
-        private $_password = "mysql";
+        private $_password = "";
         private $_database = "partify";
     
         public static function getInstance() {
