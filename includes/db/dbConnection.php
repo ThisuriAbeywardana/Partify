e2f56ba452c2e07b8bc5759312607eaf4e4312cb
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-02-06 12:59:19 +0530

    chnged db name

--- includes/db/dbConnection.php
+++ includes/db/dbConnection.php
@@ -5,7 +5,7 @@
         private $_host = "localhost";
         private $_username = "root";
         private $_password = "mysql";
-        private $_database = "1108";
+        private $_database = "parify";
     
         public static function getInstance() {
             if(!self::$_instance) { // If no instance then make one
