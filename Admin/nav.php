62a151c1d78034822e8855b05d2196ab9a4747a9
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-02-13 23:43:56 +0530

    add navigation links

--- Admin/nav.php
+++ Admin/nav.php
@@ -2,6 +2,7 @@
     <img src="../Assets/partify.gif" alt="">
     <h2>Partify</h2>
     <ul>
+        <li><a href="../index.php">Home</a></li>
         <li><a href="./dashboard.php">Dashboard</a></li>
         <li><a href="./users.php">Users</a></li>
         <li><a href="./vendors.php">Vendors</a></li>
--- User/nav.php
+++ User/nav.php
@@ -2,9 +2,9 @@
     <img src="../Assets/partify.gif" alt="">
     <h2>Partify</h2>
     <ul>
+        <li><a href="../index.php">Home</a></li>
         <li><a href="./dashboard.php">Dashboard</a></li>
         <li><a href="./bookings.php">My Bookings</a></li>
-        
         <li><a href="./account.php">Account</a></li>
     </ul>
 </div>    
