8a3492e55d00abbbd7b223ea279670419a3b231e
Yohombu Abeysinghe <yohombuda@gmail.com>
2021-02-14 17:53:31 +0530

    password hash comparison

--- Controls/loginControl.php
+++ Controls/loginControl.php
@@ -10,30 +10,37 @@
         $username=validate($_POST['uname']);
         $pass=validate($_POST['psw']);
         
-        $sql="SELECT userId,userType FROM user WHERE username='$username' AND password='$pass'";
+        $sql="SELECT * FROM user WHERE username='$username'";
         $res=mysqli_query($connection,$sql);
         $count=mysqli_num_rows($res)>0;
         if($count>0){
             $row=$res->fetch_assoc();
-            $_SESSION['userId']=$row['userId'];
-            $_SESSION['username']=$username;
-            $_SESSION['userType']=$row['userType'];
-            $_SESSION['loggedIn']='true';
-            switch ($_SESSION['userType']){
-                case 'User':
-                    header("location: ../User/dashboard.php");
-                    break;
-                case 'Admin':
-                    header("location: ../Admin/dashboard.php");
-                    break;
-                case 'Vendor':
-                    header("location: ../Vendor/dashboard.php");
-                    break;
-                default:
-                   
-                    break;
-            }
-        }else{
+            $hash=$row['password'];
+            if(password_verify($pass,$hash)){
+                $_SESSION['userId']=$row['userId'];
+                $_SESSION['username']=$username;
+                $_SESSION['userType']=$row['userType'];
+                $_SESSION['loggedIn']='true';
+                switch ($_SESSION['userType']){
+                    case 'User':
+                        header("location: ../User/dashboard.php");
+                        break;
+                    case 'Admin':
+                        header("location: ../Admin/dashboard.php");
+                        break;
+                    case 'Vendor':
+                        header("location: ../Vendor/dashboard.php");
+                        break;
+                    default:
+                    
+                        break;
+            }}else{
+                $_SESSION['status']='Wrong Username/Password';
+                header("Location: ../login.php"); 
+             
+            
+            
+        }}else{
            $_SESSION['status']='Wrong Username/Password';
            header("Location: ../login.php"); 
         }
